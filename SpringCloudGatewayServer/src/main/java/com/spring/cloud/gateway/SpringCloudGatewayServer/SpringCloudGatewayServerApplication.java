844d2641fd57d99605f497433e603a9435f6f15c
olusolaag <olusolaagboola10@gmail.com>
2022-04-21 16:46:57 -0400

    cloud gateway updated

--- SpringCloudGatewayServer/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayServer/SpringCloudGatewayServerApplication.java
+++ SpringCloudGatewayServer/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayServer/SpringCloudGatewayServerApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.spring.cloud.gateway.SpringCloudGatewayServer.config.RedisHashComponent;
@@ -20,6 +21,7 @@ import com.spring.cloud.gateway.SpringCloudGatewayServer.dto.ApiKey;
 import com.spring.cloud.gateway.SpringCloudGatewayServer.util.AppConstants;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudGatewayServerApplication {
 	
 	@Autowired
--- SpringCloudGatewayServer/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayServer/config/RedisConfig.java
+++ SpringCloudGatewayServer/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayServer/config/RedisConfig.java
@@ -3,11 +3,13 @@ package com.spring.cloud.gateway.SpringCloudGatewayServer.config;
 import java.util.Objects;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+@Configuration
 public class RedisConfig {
 	private final Environment env;
 	
